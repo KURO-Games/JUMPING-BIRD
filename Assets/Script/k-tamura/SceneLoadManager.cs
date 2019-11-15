da259c56714f2e717afcefea7a015970567f47e8
tamura <kuro@kuro.games>
2019-11-15 13:57:11 +0900

    SceneManager書きかけ

---
+++ Assets/Script/k-tamura/SceneLoadManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneLoadManager : SingletonMonoBehaviour<SceneLoadManager>
+{
+    private Color color;
+    private float SceneSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private static void FadeOut()
+    {
+
+    }
+    private static void FadeIn()
+    {
+
+    }
+    public static void LoadScene(string SceneName)
+    {
+
+    }
+    IEnumerator FadeIn()
+    {
+       
+    }
+    IEnumerator FadeOut()
+    {
+
+    }
+}
