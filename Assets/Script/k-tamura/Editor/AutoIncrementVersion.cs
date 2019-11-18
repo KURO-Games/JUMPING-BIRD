9a6ac1bfdf3460cf8eb4807a69df794aa52da5d5
KURO-Games <kuro@kuro.games>
2019-11-18 18:41:50 +0900

    バージョン管理用スクリプト追加

---
+++ Assets/Script/k-tamura/Editor/AutoIncrementVersion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class AutoIncrementVersion
+{
+	[PostProcessBuild]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+	{
+
+		if (target == BuildTarget.iOS)
+		{
+			//0 -> 1
+			var buildNumber = int.Parse(PlayerSettings.iOS.buildNumber) + 1;
+			PlayerSettings.iOS.buildNumber = "" + buildNumber;
+		}
+	}
+}
--- Assets/Script/k-tamura/SceneManagers/TitleScene.cs
+++ Assets/Script/k-tamura/SceneManagers/TitleScene.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_IOS
+using System.Runtime.InteropServices;
+#endif
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class TitleScene : MonoBehaviour
 {
     bool Tap;
+    [SerializeField]
+    Text version;
+    [SerializeField]
+    string verstr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (verstr != "")
+            version.text = "Ver: " + verstr + "." + Application.version+"."+GetBuildNumber();
+        else
+            version.text = "Ver: " + Application.version+"."+GetBuildNumber();
     }
 
     // Update is called once per frame
@@ -20,4 +35,18 @@ public class TitleScene : MonoBehaviour
             SceneLoadManager.LoadScene("Game");
         }
     }
+    public static string GetBuildNumber()
+    {
+        #if UNITY_EDITOR
+            return PlayerSettings.iOS.buildNumber;
+        #elif UNITY_IOS
+            return GetBundleVersion();
+        #else
+            return null;
+        #endif
+    }
+    #if UNITY_IOS
+        [DllImport("__Internal")]
+        static extern string GetBundleVersion();
+    #endif
 }
