88a6b1381ae05ee41c2df754c0a6722cd67cb457
IwasakiTomoya <tomoti2525@yahoo.co.jp>
2019-11-29 15:34:29 +0900

    スクリプトの改善や、テスト。

--- Assets/Script/Finger.cs
+++ Assets/Script/Finger.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using System;
 
 public class Finger : MonoBehaviour
-{
-    public GameObject Bird;
+{    
+    private GameObject Bird;
     private bool Over = false;
     private bool Limit = true;
     private readonly float yLimit = 5;
     Vector3 rotat;
     private bool limitPos = true;
-    private float fingerPos = 0;    
-
+    private float fingerPos = 0;
+    
     void Start()
     {
-        Bird = GameObject.Find("Bird");        
+        Bird = GameObject.FindGameObjectWithTag("Bird");
     }
 
     void JumpLimitSet()
@@ -25,6 +25,7 @@ public class Finger : MonoBehaviour
 
     void PositionYReset()
     {
+        Debug.Log("aaaaa");
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
 
@@ -35,20 +36,17 @@ public class Finger : MonoBehaviour
 
     private void Update()
     {
-        rotat.z = Angle(this.transform.position,Bird.transform.position);
+        rotat.z = AngleCal(this.transform.position,Bird.transform.position);
         this.transform.rotation=Quaternion.Euler(rotat);
-    }
-    void FixedUpdate()
-    {
         if (!Over)
-        {
+        {            
             Vector3 birdPos = Bird.transform.position;
             Debug.Log("MouseDown");
             GetComponent<SpringJoint2D>().connectedAnchor = Bird.transform.position;
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(birdPos.x, birdPos.y, -5f);                        
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
-            
+            transform.position = new Vector3(mousePos.x, mousePos.y, -5f);
+            //GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+
         }
         else
         {
@@ -68,29 +66,16 @@ public class Finger : MonoBehaviour
             //Debug.Log("MouseUp");
             setBool();
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-            Bird.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;                        
+            Bird.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
             Destroy(GetComponent<SpriteRenderer>());
             Invoke("PositionFix", 0.1f);
-            Invoke("JumpLimitSet", 0.5f);              
+            Invoke("JumpLimitSet", 0.5f);
         }
-        if (Math.Abs(gameObject.transform.position.y) > yLimit)
-        {            
-            if (limitPos)
-            {
-                limitPos = false;
-                fingerPos = Mathf.Round(gameObject.transform.position.y);
-                Debug.Log(fingerPos);
-            }
-            gameObject.transform.position = new Vector2(gameObject.transform.position.x, fingerPos);
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
-            Invoke("PositionYReset", 0.5f);
-        }
-        else if (Math.Abs(gameObject.transform.position.y) < yLimit)
-        {
-            limitPos = true;
-        }
-    }
-    private float Angle(Vector2 startpos,Vector2 targetpos)
+
+        //画面外にマウスを飛ばしてもゲーム画面内に矢印が残る処理。
+        FingerLimit();
+    }   
+    private float AngleCal(Vector2 startpos,Vector2 targetpos)
     {
         Vector2 dt = targetpos - startpos;
         float rad = Mathf.Atan2(dt.y, dt.x);
@@ -104,6 +89,26 @@ public class Finger : MonoBehaviour
         Bird.GetComponent<Bird>().MousePush = false;
         Bird.GetComponent<Bird>().Fly = true;
         Over = true;
+    }
+
+    private void FingerLimit()
+    {
+        if (Math.Abs(gameObject.transform.position.y) > yLimit)
+        {
+            if (limitPos)
+            {
+                limitPos = false;
+                fingerPos = Mathf.Round(gameObject.transform.position.y);
+                //Debug.Log(fingerPos);
+            }
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, fingerPos);
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            Invoke("PositionYReset", 0.5f);
+        }
+        else if (Math.Abs(gameObject.transform.position.y) < yLimit)
+        {
+            limitPos = true;
+        }
     }
 }
 
