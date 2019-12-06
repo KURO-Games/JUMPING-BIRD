da4d78c971e6ffd70340f4a77d96efe640dea038
kousei1026 <>
2019-12-06 14:22:29 +0900

    kousei1026
    Backボタンの追加
    鳥のHP回復

---
+++ Assets/Script/Back_Yes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back_Yes : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Title");
+        Time.timeScale = 1;
+    }
+}
