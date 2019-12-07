52c00b11ec55df39f61a28f60f7f0a45d1a8812c
KURO-Games <kuro@kuro.games>
2019-12-07 20:02:33 +0900

    アニメーションコントローラー

---
+++ Assets/Script/k-tamura/AnimationController/Bird/BirdAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class BirdAnimationController : SingletonMonoBehaviour<BirdAnimationController>
+{
+    private Animator _anim;
+    private void Start()
+    {
+        _anim = this.gameObject.GetComponent<Animator>();
+    }
+    public enum BirdAnimParam
+    {
+        Flying,
+        Fall,
+        Flyup,
+        HitsStone,
+        Ricochet,
+        Swoop,
+        ZonbieHit,
+    }
+
+    public static void BirdAnimations(BirdAnimParam _BirdAnimParam)
+    {
+        Instance._anim.SetInteger("", (int)_BirdAnimParam);
+    }
+}
