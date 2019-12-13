e52d2789decb1801c2d97d479c96ce989801c426
KURO-Games <kuro@kuro.games>
2019-12-13 10:46:48 +0900

    画像追加

---
+++ Assets/Script/k-tamura/Manager/DisplayManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayManager : SingletonMonoBehaviour<DisplayManager>
+{
+    [SerializeField]
+    private Sprite _clear, _fail;
+
+
+    private void Start()
+    {
+        Instance.DispInit();
+    }
+    public void DispMgr(bool Clear)
+    {
+        if (Clear)
+            this.gameObject.GetComponent<Image>().sprite = _clear;
+        else
+            this.gameObject.GetComponent<Image>().sprite = _fail;
+        this.gameObject.GetComponent<Image>().color = new Vector4(1, 1, 1, 1);
+    }
+    public void DispInit()
+    {
+        this.gameObject.GetComponent<Image>().sprite = null;
+        this.gameObject.GetComponent<Image>().color = new Vector4(1, 1, 1, 0);
+    }
+    
+}
