8f4f998f5f40692cb4a3afc3e4c9a0704bf264a2
KURO-Games <kuro@kuro.games>
2019-12-13 11:07:00 +0900

    goal処理

---
+++ Assets/Script/k-tamura/Manager/GoleFlags.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoleFlags : SingletonMonoBehaviour<GoleFlags>
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Vector2 ThisPosition()
+    {
+        return this.gameObject.transform.position;
+    }
+}
