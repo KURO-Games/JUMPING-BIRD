5dac5664b83dbd4e917de82a1b6a3c106bfc6e10
IwasakiTomoya <tomoti2525@yahoo.co.jp>
2020-01-06 18:50:35 +0900

    雲を地面と別でループさせ、臨場感を出した。

---
+++ Assets/Script/Iwasaki/CloudRoop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudRoop : MonoBehaviour
+{
+    private int num = 1;
+    [SerializeField]
+    private GameObject mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-0.05f, 0, 0);
+        if (transform.position.x < mainCamera.transform.position.x - 20.3f)
+        {
+            transform.position = new Vector3(mainCamera.transform.position.x + 20.3f, 1, 10);
+        }
+    }
+}
