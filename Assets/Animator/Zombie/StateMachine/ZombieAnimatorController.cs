2d156c0935d0c36399ddfd3ccc17dac9b33afaeb
KURO-Games <kuro@kuro.games>
2020-01-20 18:02:55 +0900

    ゾンビモーション変更、Bird過去データ消去

---
+++ Assets/Animator/Zombie/StateMachine/ZombieAnimatorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimatorController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+}
---
+++ Assets/Animator/Zombie/StateMachine/ZombieStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ZombieStateMachine : StateMachineBehaviour
+{
+    bool _once;
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.normalizedTime < 1.0f && !_once)
+        {
+            animator.SetTrigger("Once");
+            _once = true;
+        }
+
+    }
+}
