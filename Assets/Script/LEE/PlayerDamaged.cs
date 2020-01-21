74a12d98de6c8f7f73332fb1ef429387721807f7
congibab <49512074+congibab@users.noreply.github.com>
2020-01-22 00:01:03 +0900

    blink効果のcoroutine追加

--- Assets/Script/LEE/PlayerDamaged.cs
+++ Assets/Script/LEE/PlayerDamaged.cs
@@ -6,30 +6,47 @@ using UnityEngine.UI;
 public class PlayerDamaged : MonoBehaviour
 {
     private Image image;
-    [Range(1, 10), SerializeField]
-    private int fadeOutSpeed = 1;
-    private bool isDamaged;
+    [Range(0, 5), SerializeField]
+    private float WaitTime = 0.5f;
+    //[Range(1, 10), SerializeField]
+    //private int fadeOutSpeed = 1;
+    //private bool isDamaged;
 
     // Start is called before the first frame update
     void Start()
     {
         image = GetComponent<Image>();
-        StartCoroutine("fadeout");
+        StartCoroutine("blink");
 
     }
 
-    IEnumerator fadeout ()
+    //IEnumerator fadeout()
+    //{
+    //    yield return new WaitUntil(() => Bird.Instance.isDamaged);
+    //    float alpha = 1f;
+    //    while (alpha > 0)
+    //    {
+    //        yield return new WaitForSeconds(0.1f);
+    //        alpha -= Time.deltaTime * fadeOutSpeed;
+    //        image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+
+    //    }
+    //    Bird.Instance.isDamaged = false;
+    //    StartCoroutine("fadeout");
+    //}
+
+    IEnumerator blink ()
     {
         yield return new WaitUntil(() => Bird.Instance.isDamaged);
-        float alpha = 1f;
-        while (alpha > 0)
-            {
-                yield return new WaitForSeconds(0.1f);
-                alpha -= Time.deltaTime * fadeOutSpeed;
-                image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
-               
-            }
+        //float alpha = 1f;
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+
+        yield return new WaitForSeconds(WaitTime);
+
+        //alpha = 0;
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+
         Bird.Instance.isDamaged = false;
-        StartCoroutine("fadeout");
+        StartCoroutine("blink");
     }
 }
