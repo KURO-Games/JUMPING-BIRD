4e972cd65a9269339fc6a373bc3fa9ac088affe8
KURO-Games <kuro@kuro.games>
2020-01-24 14:58:54 +0900

    ZombieState途中

---
+++ Assets/Script/k-tamura/ZombieState/ZombieState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieState : MonoBehaviour
+{
+    public float HitPoint;
+    public float RockStr;
+    public enum ZombieStatus
+    {
+        Default,
+        Red,
+        Green,
+        Big
+    }
+    public ZombieStatus _zombieStatus;
+    private void Start()
+    {
+        switch (_zombieStatus)
+        {
+            case ZombieStatus.Big:
+                HitPoint = 2;
+                RockStr = 2;
+                break;
+            case ZombieStatus.Red:
+                HitPoint = 1;
+                RockStr = 2;
+                break;
+            case ZombieStatus.Green:
+                HitPoint = 2;
+                RockStr = 1;
+                break;
+            case ZombieStatus.Default:
+                HitPoint = 1;
+                RockStr = 1;
+                break;
+        }
+
+    }
+}
