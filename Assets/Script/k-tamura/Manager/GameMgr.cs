379aa85e7f1e4d3a7cc7deea38887549d841520b
KURO-Games <kuro@kuro.games>
2020-01-24 09:54:31 +0900

    ゲームマネージャー変更 ゲームスタート時にサウンドを流す。

--- Assets/Script/k-tamura/Manager/GameMgr.cs
+++ Assets/Script/k-tamura/Manager/GameMgr.cs
@@ -6,5 +6,13 @@ using UnityEditor;
 public class GameMgr : SingletonMonoBehaviour<GameMgr>
 {
     [Header("BirdParamater")]
-    public float BirdJumpPower, BirdJumpMaxPosition, BirdJumpMinPosition;
+    public float BirdJumpPower;
+    public float BirdJumpMaxPosition, BirdJumpMinPosition;
+    [Header("ZombieParamater")]
+    public int ZombieIt;
+
+    private void Start()
+    {
+        SoundManager.PlayBgm(BGM.Settings);
+    }
 }
