0f742135ce5c0c285e01ff7c6385fb57dea4e590
KURO-Games <kuro@kuro.games>
2020-01-24 13:46:01 +0900

    消去追加

--- Assets/Script/k-tamura/AnimationController/Building/BuildingColiderAnimator.cs
+++ Assets/Script/k-tamura/AnimationController/Building/BuildingColiderAnimator.cs
@@ -10,15 +10,37 @@ public class BuildingColiderAnimator : MonoBehaviour
         BuildingRight,
         BuildingUnder
     }
+
     [SerializeField, Header("アニメーション")]
     GameObject Building;
     [SerializeField, Header("ビルBoxColider設定")]
     BoxColiders BoxColiderChoice;
 
+    private float mLength;
+    private float mCur;
+    private bool destroy;
+    private void Start()
+    {
+        Animator animOne = Building.GetComponent<Animator>();
+        AnimatorStateInfo infAnim = animOne.GetCurrentAnimatorStateInfo(0);
+        mLength = infAnim.length;
+        mCur = 0;
+    }
+    private void Update()
+    {
+        if (!destroy)
+            return;
+        mCur += Time.deltaTime;
+        if (mCur > mLength)
+        {
+            Destroy(Building);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Bird")
         {
+            destroy = true;
             SoundManager.Instance.PlaySe(SE.AttackBuilding);
             Building.GetComponent<Animator>().SetTrigger(BoxColiderChoice.ToString());
         }
