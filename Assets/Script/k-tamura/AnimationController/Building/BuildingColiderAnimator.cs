65d24e58f105e0eb4fb46f815d85b539eb6caa5d
KURO-Games <kuro@kuro.games>
2020-01-24 13:04:04 +0900

    ゾンビアニメーション、ビルアニメーション更新

---
+++ Assets/Script/k-tamura/AnimationController/Building/BuildingColiderAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingColiderAnimator : MonoBehaviour
+{
+    enum BoxColiders
+    {
+        BuildingLeft,
+        BuildingRight,
+        BuildingUnder
+    }
+    [SerializeField, Header("ビルBoxColider設定")]
+    BoxColiders BoxColiderChoice;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bird")
+        {
+            transform.root.gameObject.GetComponent<Animator>().SetTrigger(BoxColiderChoice.ToString());
+        }
+    }
+
+}
---
+++ Assets/Script/k-tamura/Test/AnimationTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.SceneManagement;
+
+public class AnimationTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject AnimationObject;
+    [SerializeField]
+    private string[] animationName;
+    private int _num;
+
+    // Start is called before the first frame update
+    public void NextButton()
+    {
+        if (_num >= animationName.Length) _num = 0;
+        AnimationObject.GetComponent<Animator>().SetTrigger(animationName[_num]);
+        _num++;
+    }
+    public void ResetButton()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().name));
+    }
+
+}
