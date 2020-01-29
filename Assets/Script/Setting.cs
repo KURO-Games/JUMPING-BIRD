8bbed2990436cb3f6dfb3678de9d568ca56e4532
congibab <49512074+congibab@users.noreply.github.com>
2020-01-30 01:19:53 +0900

    Prefab化して（鳥が死ぬ時、動くバグ、メニュを開いた後鳥が動くバグ）修正完了、メニュ画面をゲーム画面の一番前に配置

--- Assets/Script/Setting.cs
+++ Assets/Script/Setting.cs
@@ -7,13 +7,16 @@ public class Setting : MonoBehaviour
     [SerializeField]
     private GameObject GoTitle;
     [SerializeField]
-    private GameObject Bird;
-    [SerializeField]
     private GameObject SPIcon;
+
+    CanvasGroup canvas;
 
     void Start()
     {
         //GoTitle.gameObject.SetActive(false);        
+        canvas = GetComponent<CanvasGroup>();
+        canvas.blocksRaycasts = true;
+        StartCoroutine("Enable");
     }
 
     public void OnClick()
@@ -21,10 +24,16 @@ public class Setting : MonoBehaviour
         SoundManager.PlayBgm(BGM.Settings);
         GoTitle.gameObject.SetActive(true);
         Time.timeScale = 0;
-        Bird.GetComponent<BirdJumper>().enabled = false;
+        Bird.Instance.GetComponent<BirdJumper>().enabled = false;
         SPIcon.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        
     }
-  
 
+    IEnumerator Enable()
+    {
+        yield return new WaitUntil(() => Bird.Instance.Die);
+        canvas.blocksRaycasts = false;
+
+    }
 
 }
