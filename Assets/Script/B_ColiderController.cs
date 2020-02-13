d91f737a1d63fb855628efcf65a611c65526087a
congibab <49512074+congibab@users.noreply.github.com>
2020-02-13 17:40:58 +0900

    test Script

---
+++ Assets/Script/B_ColiderController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_ColiderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bird")
+        {
+           transform.GetChild(3).gameObject.SetActive(false);
+
+            StartCoroutine("Delay");
+        }
+        else
+        {
+        
+            return;
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.1f);
+        transform.GetChild(3).gameObject.SetActive(true);
+
+    }
+}
